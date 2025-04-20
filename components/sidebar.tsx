cc43776e466022b7fa592ea1debc94a078df6396
WolfGamer2 <rushilchopra@gmail.com>
2025-04-20 10:54:15 -0700

    responsive sidebar i hope

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -267,8 +267,8 @@ export function Sidebar() {
   return (
     <aside
       className={cn(
-        "fixed inset-y-0 left-0 z-20 flex w-64 flex-col border-r bg-background transition-all duration-300",
-        isOpen ? "md:w-64 translate-x-0" : "md:w-16 -translate-x-0",
+        "fixed inset-y-0 left-0 z-20 flex flex-col border-r bg-background transition-all duration-300",
+        isOpen ? "w-64" : "w-16"
       )}
     >
       {sidebarContent}
